f32f166c3798738e546e85248ef8d8fab9f11fb2
Vaibhav Agarwal <iamvaibhav.agarwal@gmail.com>
2023-09-23 16:18:17 +0530

    fix

--- db.js
+++ db.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 async function db() {
   try {
     const connect = await mongoose.connect(process.env.MONGO_URI);
+    
     console.log(`MongoDB Connected: ${connect.connection.host}`.cyan);
   } catch (error) {
     console.error(`MongoDB Connection Error: ${error.message}`.red);
